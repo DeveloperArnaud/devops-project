d8fee5dd36f547d248edadd391709a8e80f65d89
Arnaud Coquerelle <arnaud.coquerelle@bayard-presse.com>
2021-11-07 19:56:50 +0100

    test

--- connection.js
+++ connection.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 const config = require('./conf/default')
+require('dotenv').config()
 
 var mySqlConnection = mysql.createConnection({
-    host: config.mysql.host,
-    user: config.mysql.user,
-    password: config.mysql.password,
-    database: config.mysql.database,
+    host: config.mysql.host || process.env.DB_HOST,
+    user: config.mysql.user || process.env.DB_USER,
+    password: config.mysql.password || process.env.DB_USER,
+    database: config.mysql.database || process.env.DB_USER,
     multipleStatements: true
 })
 
