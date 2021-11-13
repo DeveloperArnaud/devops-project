f6e221a2eead76230439dd1e81a7c778bd9aa766
Arnaud Coquerelle <arnaud.coquerelle@bayard-presse.com>
2021-11-13 01:08:29 +0100

    test

--- test/config.js
+++ test/config.js
@@ -4,7 +4,7 @@ const config = require('../conf/default')
 describe('Configuration', () => {
     let configTest = {
         "mysql": {
-            "host": "localhost",
+            "host": "127.0.0.&",
             "user": "root",
             "password": "Gwada97195",
             "database": "company"
